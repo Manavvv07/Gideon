2095999eb6c18cb39c943953e164fd71bad96a82
Manavvv07 <manavsingh29sk@gmail.com>
2025-12-08 14:33:30 +0530

    UI fixes for answer component

--- frontend/src/components/Answer.jsx
+++ frontend/src/components/Answer.jsx
@@ -1,9 +1,38 @@
-const Answer = ({text}) => {
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+const Answer = ({ text }) => {
   return (
     <div className='answer-container'>
-        <p className='answer'>{text}</p>
+      <div className='answer'>
+        <ReactMarkdown
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '');
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  style={dracula}
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                  customStyle={{ borderRadius: "5px", margin: "10px 0" }} 
+                >
+                  {String(children).replace(/\n$/, '')}
+                </SyntaxHighlighter>
+              ) : (
+                <code className={className} style={{background: "#333", padding: "2px 5px", borderRadius: "3px"}} {...props}>
+                  {children}
+                </code>
+              );
+            }
+          }}
+        >
+          {text}
+        </ReactMarkdown>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Answer
+export default Answer;
